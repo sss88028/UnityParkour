37af3af34b63e1e5e5df14f13b79e55f7e915522
JackFunfia <jack.tu@funfia.com>
2023-02-06 14:26:26 +0800

    Add simple fall

--- UnityParkour/Assets/Scripts/PlayerController/PlayerController.cs
+++ UnityParkour/Assets/Scripts/PlayerController/PlayerController.cs
@@ -13,9 +13,23 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	private Animator _playerAnimator;
 	[SerializeField]
-	private string _paramertName = "MoveAmount";
+	private string _parameterName = "MoveAmount";
+	[SerializeField]
+	private CharacterController _characterController;
+
+	[Header("GroundCheck")]
+	[SerializeField]
+	private float _groundCheckRadius;
+	[SerializeField]
+	private Vector3 _groundCheckOffset;
+	[SerializeField]
+	private LayerMask _groundCheckLayer;
+
+	[SerializeField]
+	private Vector3 _gravity = new Vector3(0, -9.8f, 0);
 
 	private Transform _currentTransform;
+	private Vector3 _fallSpeed = Vector3.zero;
 	#endregion private-field
 
 	#region MonoBehaviour-method
@@ -29,7 +43,23 @@ public class PlayerController : MonoBehaviour
 		UpdatePos();
 	}
 
-	private void OnDestroy()
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+		var oldColor = Gizmos.color;
+		Gizmos.color = Color.red;
+		if (GroundCheck())
+		{
+			Gizmos.color = Color.green;
+		}
+		Gizmos.DrawSphere(transform.TransformPoint(_groundCheckOffset), _groundCheckRadius);
+
+		Gizmos.color = oldColor;
+
+	}
+#endif
+
+    private void OnDestroy()
 	{
 		CameraTransformProvider.Instance.OnGetTransform -= OnGetTransform;
 	}
@@ -44,16 +74,29 @@ public class PlayerController : MonoBehaviour
 		var moveAmount = Math.Abs(h)+ Math.Abs(v);
 		var moveInput = (new Vector3(h, 0, v)).normalized;
 
-		if (moveAmount > 0)
+		var isGround = GroundCheck();
+		var moveDir = GetCameraRotation() * moveInput;
+
+		var velocity = moveDir * _speed;
+		if (!isGround)
 		{
-			var moveDir = GetCameraRotation() * moveInput;
-
-			transform.position += moveDir * _speed * Time.deltaTime;
-			LerpRotation(moveDir);
+			_fallSpeed += _gravity * Time.deltaTime;
 		}
+		else 
+		{
+			_fallSpeed.y = -0.5f;
+		}
+		velocity.y = _fallSpeed.y;
+		_characterController.Move(velocity * Time.deltaTime);
 
-		_playerAnimator?.SetFloat(_paramertName, moveInput.magnitude, 0.2f, Time.deltaTime);
+		LerpRotation(moveDir);
 
+		_playerAnimator?.SetFloat(_parameterName, moveInput.magnitude, 0.2f, Time.deltaTime);
+	}
+
+	private bool GroundCheck() 
+	{
+		return Physics.CheckSphere(transform.TransformPoint(_groundCheckOffset), _groundCheckRadius, _groundCheckLayer);
 	}
 
 	private void LerpRotation(Vector3 moveDir)
